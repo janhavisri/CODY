c725d8fd69839e2752ba789628a38e8e07726494
janhavisri <janhavis830@gmail.com>
2022-06-04 17:22:10 +0530

    commit

--- backend/config.js
+++ backend/config.js
@@ -2,7 +2,5 @@ const api_config = {
 
     db_url: "mongodb+srv://Janhavis830:madan2830@mystorage.pxblk.mongodb.net/Dashboard?retryWrites=true&w=majority",
 
-
-};
-
+}
 module.exports = api_config;
